5c0d8d5dcd5100dde7325784b5c74c76d8c1dc74
Jordan Sullivan <jordanmsllvn@gmail.com>
2019-10-03 10:43:04 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+// @ts-ignore
+import express, { Application } from "@types/express";
+
+interface IServeEnvOptions {
+  app: Application;
+  route?: string;
+  exposedVars: string[];
+}
+
+/**
+ * Injects environment variables at runtime at route. For backend use only.
+ * @param config - configuration
+ */
+export function serveEnvConfig({
+  app,
+  route = "/env.js",
+  exposedVars
+}: IServeEnvOptions): any {
+  app.get(route, (_req: any, res: any) => {
+    res.set("Content-Type", "application/javascript");
+    let body = `
+      window.env = {${(() => {
+        const vars = [];
+        for (const eVar of exposedVars) {
+          if (process.env[eVar]) vars.push(`${eVar}: ${process.env[eVar]}`);
+        }
+        return vars.join(",");
+      })()}};
+    `;
+    res.send(body);
+  });
+}
+
+/**
+ * Loads environment variables from runtime or compile time variables. For
+ * Frontend use only.
+ */
+export function getEnvConfig() {
+  // @ts-ignore
+  if (window && window.env) {
+    // @ts-ignore
+    if (process && process.env) return { ...process.env, ...window.env };
+    // @ts-ignore
+    else return window.env;
+  } else if (process && process.env) return process.env;
+}
