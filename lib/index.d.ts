ff5e7c3b2eda10043cc334fd5d3682aa55724d5e
jordanmsllvn <jmsull@protonmail.com>
2021-02-03 00:48:14 -0700

    getting ready for repo based package install

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Injects environment variables at runtime at route. For backend use only.
+ * @param config - configuration
+ */
+export declare function serveEnvConfig(exposedVars: string[]): any;
+/**
+ * Loads environment variables from runtime or compile time variables. For
+ * Frontend use only.
+ */
+export declare function getEnvConfig(): any;
